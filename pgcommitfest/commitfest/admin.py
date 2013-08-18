d7c16a94fbfbb866fbe2a8575f06efe6e2748f30
Magnus Hagander <magnus@hagander.net>
2013-08-18 17:54:54 +0200

    Add some more admin views

--- pgcommitfest/commitfest/admin.py
+++ pgcommitfest/commitfest/admin.py
@@ -11,8 +11,14 @@ class PatchAdmin(admin.ModelAdmin):
 	list_display = ('name', )
 #	list_filter = ('commitfests_set__commitfest__name',)
 
+class MailThreadAttachmentAdmin(admin.ModelAdmin):
+	list_display = ('date', 'author', 'messageid', 'mailthread',)
+
 admin.site.register(Committer)
 admin.site.register(CommitFest)
 admin.site.register(Topic)
 admin.site.register(Patch, PatchAdmin)
 admin.site.register(PatchHistory)
+
+admin.site.register(MailThread)
+admin.site.register(MailThreadAttachment, MailThreadAttachmentAdmin)
